90449d34e6e30628c79da2dea2cabdbe0decd1ad
Patrick Ayebazibwe <104129127+Patrck23@users.noreply.github.com>
2024-12-21 21:59:50 +0300

    sale struct update

--- internals/models/saleRegistration/sale.go
+++ internals/models/saleRegistration/sale.go
@@ -3,19 +3,20 @@ package saleRegistration
 import (
 	"car-bond/internals/models/carRegistration"
 	"car-bond/internals/models/companyRegistration"
+
 	"gorm.io/gorm"
 )
 
 type Sale struct {
 	gorm.Model
 	TotalPrice    float64                     `gorm:"type:numeric;not null" json:"total_price"`
-	SaleDate 	  string                      `gorm:"type:date;not null" json:"sale_date"`
-	CarID 		  int                         `gorm:"references:ID"`
-	Car  		  carRegistration.Car  		  `gorm:"foreignKey:CarID"`
+	SaleDate      string                      `gorm:"type:date;not null" json:"sale_date"`
+	CarID         int                         `gorm:"references:ID"`
+	Car           carRegistration.Car         `gorm:"foreignKey:CarID"`
 	CompanyID     int                         `gorm:"references:ID"`
 	Company       companyRegistration.Company `gorm:"foreignKey:CompanyID"`
-	IsFullPayment bool               		  `json:"is_full_payment"`
-	PaymentPeriod int 						  `json:"payment_period`
+	IsFullPayment bool                        `json:"is_full_payment"`
+	PaymentPeriod int                         `json:"payment_period"`
 	CreatedBy     string                      `gorm:"size:100" json:"created_by"`
 	UpdatedBy     string                      `gorm:"size:100" json:"updated_by"`
 }
--- internals/models/saleRegistration/sale.payment.mode.go
+++ internals/models/saleRegistration/sale.payment.mode.go
@@ -6,10 +6,10 @@ import (
 
 type SalePaymentMode struct {
 	gorm.Model
-	ModeOfPayment   string              `json:"mode_of_payment"`
-	transactionId 	string 				`gorm:"unique" json:"transaction_id"`
-	SalePaymentID   uint                	`gorm:"references:ID"`
-	SalePayment  	SalePayment  		`gorm:"foreignKey:SalePaymentID;references:ID" json:"sale_payment"`
-	CreatedBy    	string              `gorm:"size:100" json:"created_by"`
-	UpdatedBy    	string              `gorm:"size:100" json:"updated_by"`
+	ModeOfPayment string      `json:"mode_of_payment"`
+	TransactionID string      `gorm:"unique" json:"transaction_id"`
+	SalePaymentID uint        `gorm:"references:ID"`
+	SalePayment   SalePayment `gorm:"foreignKey:SalePaymentID;references:ID" json:"sale_payment"`
+	CreatedBy     string      `gorm:"size:100" json:"created_by"`
+	UpdatedBy     string      `gorm:"size:100" json:"updated_by"`
 }
