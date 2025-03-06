7f26b9896a855dc2339f770e0e4e72aa8f2cc015
Patrick Ayebazibwe <104129127+Patrck23@users.noreply.github.com>
2025-03-06 07:12:48 +0300

    transaction update

--- internals/models/alertRegistration/transaction.go
+++ internals/models/alertRegistration/transaction.go
@@ -1,6 +1,8 @@
 package alertRegistration
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -8,9 +10,25 @@ type Transaction struct {
 	gorm.Model
 	CarChasisNumber string `gorm:"not null;index" json:"car_chasis_number"`
 	TransactionType string `gorm:"not null" json:"transaction_type"`
+	Description     string `gorm:"size:100" json:"description"`
 	FromCompanyId   uint   `gorm:"not null;index" json:"from_company_id"`
 	ToCompanyId     uint   `json:"to_company_id"`
 	CreatedBy       string `gorm:"size:100;not null" json:"created_by"`
 	UpdatedBy       string `gorm:"size:100" json:"updated_by"`
 	ViewStatus      bool   `json:"view_status"`
+}
+
+// BeforeCreate hook to set the Description based on the TransactionType
+func (t *Transaction) BeforeCreate(tx *gorm.DB) (err error) {
+	switch t.TransactionType {
+	case "InTransit":
+		t.Description = fmt.Sprintf("Car %s is currently in transit.", t.CarChasisNumber)
+	case "Storage":
+		t.Description = fmt.Sprintf("Car %s is stored at the facility.", t.CarChasisNumber)
+	case "Sold":
+		t.Description = fmt.Sprintf("Car %s has been sold.", t.CarChasisNumber)
+	default:
+		t.Description = "Transaction type not recognized."
+	}
+	return nil
 }
